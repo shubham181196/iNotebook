6449aa78f9b5622454ea4baba6b0b179620ebf49
shubham181196 <87092826+shubham181196@users.noreply.github.com>
2023-12-01 18:14:07 +0530

    db file space added

--- Backend/db.js
+++ Backend/db.js
@@ -2,6 +2,7 @@
 const mongoose=require('mongoose');
 const mongoURI='mongodb://localhost:27017/iNotebook'
 
+
 const connectToMongo=()=>{
     mongoose.connect(mongoURI)
     // console.log("moongose connected");
